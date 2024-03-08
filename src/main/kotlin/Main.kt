d9709260a6880d5dda18b58c36f6852397090937
Anthony <anthonyp235@hotmail.com>
2024-03-08 08:43:32 -0500

    Fixed root folder

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val myFirstVariable: Boolean = true
+    println(myFirstVariable)
+    val mySecondVariable = 2
+    println(mySecondVariable)
+    var name = "Anthony"
+    var age = 23
+    val mystring = """
+        Hi my name is $name
+        and I am $age years old
+    """
+    println(mystring)
+}
+//code styles Inspection Profiles .gitignore .name compiler.xml gradle.xml gradle.xml jarRepositories.xml kotlinc.xml
